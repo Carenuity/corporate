32fb6689e1c5a1311237899edbe0c000e68d3d06
mevica <mevicandanu@gmail.com>
2024-04-19 21:24:28 +0300

    Udating sq-power page

--- pages/sq-power.tsx
+++ pages/sq-power.tsx
@@ -30,8 +30,121 @@ const Page = () => {
       <section className="pt-4">
         <div className="container">
           <div className="row">
-            {/* You can start from here but you are flexible should you want to get creative with the page */}
-            SQ-POWER
+            <h5>High quality wall mount power module</h5>
+            <br />
+            <div className="col-lg-6  mb-5">
+              <img
+                src="/img/content/sq-power-front.png"
+                className="mt-1-9 wow fadeInUp"
+                data-wow-delay="100ms"
+                alt="..."
+                style={{
+                  visibility: 'visible',
+                  animationDelay: '100ms',
+                  animationName: 'fadeIn',
+                }}
+              />
+            </div>
+            <div className="col-lg-6  mb-5">
+              <img
+                src="/img/content/sq-power-back.png"
+                className="mt-1-9 wow fadeInUp"
+                data-wow-delay="100ms"
+                alt="..."
+                style={{
+                  visibility: 'visible',
+                  animationDelay: '100ms',
+                  animationName: 'fadeIn',
+                }}
+              />
+            </div>
+           
+        
+           {/* <div
+                  className="e-card"
+                  style={{
+                    boxShadow: '3px 3px 15px 3px  hsla(0, 0%, 80%)',
+                    height: '300px',
+                  }}
+                >  */}
+              <div className="container">
+                <div className="row">
+                <div className="col-lg-4 mt-n1-9 mb-5">
+                <br />
+                <h5>Basic Information</h5>
+                <b>Product series:</b>Wall mounting power module<br />
+                <b>Type:</b>Flyback<br />
+                <b>Voltage:</b>85-265 VAC 50/60Hz<br />
+                <b>Max power:</b>5W 1A<br />
+                <b>Safety standard:</b>EN 60669-2-5<br />
+                <b>Installation category:</b>III<br /> 
+                <b>Certification:</b>CE/ROHS/WEEE<br />
+                
+                </div>
+
+                <div className="col-lg-4 mt-n1-9 mb-5">
+                <br />
+                <h5>Appearance</h5>
+                <b>Weight:</b>49g<br />
+                <b>Casing materials:</b>PCLEXAN121R<br />
+
+                
+                <h5>Working Environment</h5>
+                <b>Applicable place:</b>Indoor<br />
+                <b>Working temperature:</b>-40 ~ 80 °C<br />
+                <b>Working humidity:</b>Up to 90 % RH<br /> 
+                <b>Certification:</b>CE/ROHS/WEEE<br />
+                
+                </div>
+
+                <div className="col-lg-4 mt-n1-9 mb-5">
+                <br />
+                <h5>Installation</h5>
+                <b>Installation method:</b>Flush mounting box<br />
+                <b>Installation type:</b>Screws installation<br />
+                <b>Wiring way:</b>N and L<br />
+                <b>Wire diameter:</b>0.75 – 2.5 mm2<br />
+                </div>
+                </div>
+              </div>
+              
+             
+                <hr className="mb-5 "/>
+              <div className="container">
+          <div className="row">
+            <h5 >Sq-Power Drawings</h5>
+            <br />
+            <div className="col-lg-6 ">
+    
+              <img
+                src="/img/content/sq-powerdrawing1.png"
+                className="mt-1-9 wow fadeInUp"
+                data-wow-delay="100ms"
+                alt="..."
+                style={{
+                  visibility: 'visible',
+                  animationDelay: '100ms',
+                  animationName: 'fadeIn',
+                }}
+              />
+            </div>
+            <div className="col-lg-6 ">
+              <img
+                src="/img/content/sq-powerdrawing2.png"
+                className="mt-1-9 wow fadeInUp"
+                data-wow-delay="100ms"
+                alt="..."
+                style={{
+                  visibility: 'visible',
+                  animationDelay: '100ms',
+                  animationName: 'fadeIn',
+                }}
+              />
+            </div>
+            </div>
+            </div>
+            
+
           </div>
         </div>
       </section>
