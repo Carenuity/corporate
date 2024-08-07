b91ea6d143aced9f272ad3fb2d00a219c9f4d11a
mevica <mevicandanu@gmail.com>
2024-08-07 12:36:09 -0700

    refactor:restracturing content

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home({
       <div className=" row "> 
       
       <div className="col-md-8 "> 
-      <h5>SQ-Panel now on Kickstarter</h5>
+      <h5>The SQ-Panel shows the Air Quality level you breathe </h5>
       <p>The SQ-Panel has built-in sensors for measuring air quality, temperature, humidity, pressure, carbon dioxide, TVOC and presence. 
       The SQ-Panel connects to the Carenuity cloud via the Carenuity Border Router, which acts as a gateway between 
         your home network and the internet. This enables you to monitor the sensor values and define their threshold values from anywhere, 
