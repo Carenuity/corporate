5f9601884d4a18253ba397e1a2c09432653bba75
mevica <mevicandanu@gmail.com>
2024-09-12 08:55:52 -0700

    refactor:editing content

---
+++ pages/AI-Assisted-Devices.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Head from 'next/head';
+import Link from 'next/link';
+
+// eslint-disable-next-line no-unused-vars
+const Page = ({ isMobile }: { isMobile: boolean }) => {
+  const pageTitle = 'AI-Assisted Devices';
+  return (
+    <>
+      <Head>
+        <meta
+          name="keywords"
+          content="Carenuity ChipGlobe Electronics Embedded Systems"
+        />
+        <meta
+          name="description"
+          content={`We're Delivering Peace-in-Mind by Vital and Environmental Sensing and Care that you See what No Else will See.`}
+        />
+        <title>{pageTitle}</title>
+      </Head>
+
+      {/* <!-- PAGE TITLE
+        ================================================== --> */}
+      <PageTitle
+        links={[{ path: '/', title: '' }]}
+        pageTitle={pageTitle}
+        pageUrl=""
+      />
+
+      <section className="pt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10">
+              <h3 className=" mb-1">
+              AI-Assisted Devices
+              </h3>
+        </div>
+        </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
