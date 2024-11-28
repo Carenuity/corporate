f8f2715511a0a7c8d39ca78f13c7a50d9edb5e16
Dr. Max E. Mergenthaler <info@mergenthaler.de>
2024-11-28 17:43:06 +0100

    Update level-2.tsx

--- pages/home-challenge/level-2.tsx
+++ pages/home-challenge/level-2.tsx
@@ -162,12 +162,12 @@ const Page = ({ isMobile }: { isMobile: boolean }) => {
               {''}. There you can also find detailed instructions for removing
               the metal frame and programming the C3-Mini for the first time.
               Note: For running the app, it is not necessary to enter your own
-              SSID and password, as the viewer can also be used on the specified
+              SSID and password, as the GPIO-Vviewer can also be used on the specified
               address 192.168.4.1, i.e. it does not have to be integrated into
               your home network. The password is <strong>password</strong>.
               <br />
               For Level 2, the Gerber data (ZIP) and a screenshot of your
-              smartphone must be sent by e-mail to{' '}
+              smartphone showing the GPIO-Viewer App must be sent by e-mail to{' '}
               <strong>team@carenuity.com.</strong>
             </p>
           </div>
