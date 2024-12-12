3817c1518c565e5d6c90b026e5d2d858b0e0111f
Dr. Max E. Mergenthaler <info@mergenthaler.de>
2024-12-12 09:32:00 +0100

    Update oauth.tsx

--- pages/home-challenge/oauth.tsx
+++ pages/home-challenge/oauth.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import PageTitle from '../../components/PageTitle';
 
 const Page = () => {
-  const pageTitle = 'OAUTH Home Challenge';
+  const pageTitle = 'AUTH Home Challenge';
   return (
     <>
       <Head>
