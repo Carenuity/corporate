9347437e7d97114da0041002f30e7bedcdf1d884
mevica <mevicandanu@gmail.com>
2025-03-25 15:02:33 +0300

    feat: Adding serial-number page

---
+++ pages/serial-number.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Head from 'next/head';
+import PageTitle from '../components/PageTitle';
+
+const Page = () => {
+  const pageTitle = 'Serial Number';
+  return (
+    <>
+      <Head>
+        <meta
+          name="keywords"
+          content="Carenuity ChipGlobe Electronics Embedded Systems"
+        />
+        <meta
+          name="description"
+          content={`We're Delivering Peace-in-Mind by Vital and Environmental Sensing and Care that you See what No Else will See.`}
+        />
+        <title>{pageTitle}</title>
+      </Head>
+
+      {/* <!-- PAGE TITLE
+        ================================================== --> */}
+      <PageTitle
+        links={[{ path: '/', title: '' }]}
+        pageTitle={pageTitle}
+        pageUrl=""
+      />
+
+      <section className="pt-4">
+        <div className="container">
+            <h4>Serial Number</h4>
+           {/* eslint-disable-next-line @next/next/no-img-element */}
+           <img
+                src="/img/content/serial-number/serial-number.jpeg"
+                className=" wow fadeInUp"
+                data-wow-delay="100ms"
+                alt="..."
+                style={{
+                  visibility: 'visible',
+                  animationDelay: '100ms',
+
+                  animationName: 'fadeIn',
+                }}
+              />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
