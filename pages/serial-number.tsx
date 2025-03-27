1c641eba37a6bfd4e0db137f688b093caa9b8c09
mevica <mevicandanu@gmail.com>
2025-03-27 14:45:47 +0300

    feat: updating serial number page

--- pages/serial-number.tsx
+++ pages/serial-number.tsx
@@ -27,12 +27,12 @@ const Page = () => {
       />
 
       <section className="pt-4">
-        <div className="container">
+        <div className="container ">
             <h4>Description of our Serial Number</h4>
            {/* eslint-disable-next-line @next/next/no-img-element */}
            <img
                 src="/img/content/serial-number/serial-number.jpeg"
-                className=" wow fadeInUp"
+                className=" wow fadeInUp mb-6"
                 data-wow-delay="100ms"
                 alt="..."
                 style={{
@@ -42,7 +42,31 @@ const Page = () => {
                   animationName: 'fadeIn',
                 }}
               />
+              <div>
+              <p>
+        Carenuity&apos;s system for assigning serial numbers follows a structured methodology designed for clarity, 
+        organization, and traceability. The serial number format incorporates specific identifiers that denote 
+        product type, production year, and batch sequence. This systematic approach ensures that each item is 
+        uniquely identifiable, facilitating inventory management, quality control, and customer support.
+        </p>
+        <p>The diagram illustrating this system highlights several key components:</p>
+        <ul>
+          <li><strong>Product Code:</strong>The initial segment represents the product category or model.</li>
+          <li><strong>Year Identifier: </strong>This section indicates the year of manufacture, 
+          enabling chronological tracking of production.</li>
+          <li><strong>Batch Number:</strong>A unique code for each production batch, ensuring 
+          traceability in case of recalls or quality assessments.</li>
+          <li><strong>Individual Item Number:</strong>A distinct identifier for each item 
+          within the batch, confirming its uniqueness.</li>
+        </ul>
+        <p>The visual representation simplifies understanding by mapping 
+          these segments in a clear, linear format, with color-coded sections or labels to 
+          denote each aspect of the serial number.</p>
+      <p>Carenuity's systematic approach to serial number assignment exemplifies its commitment 
+        to precision, transparency, and reliability in product management.</p>
+              </div>
         </div>
+        
       </section>
     </>
   );
