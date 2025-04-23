2cf851c1fe0a3eb23df422049156495ba2215fbb
mevica <mevicandanu@gmail.com>
2025-04-23 16:06:57 +0300

    feat: adding product launch page

--- pages/product/plant-bar.tsx
+++ pages/product/plant-bar.tsx
@@ -11,7 +11,7 @@ const Page = () => {
   return (
     <>
       <Head>
-      <Translatable />
+      {/* <Translatable /> */}
         <meta
           name="keywords"
           content="Carenuity ChipGlobe Electronics Embedded Systems"
