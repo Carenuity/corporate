fad0fbb84da6b3679d966b501f396e1ea06a3d79
Dr. Max E. Mergenthaler <info@mergenthaler.de>
2025-06-25 15:42:57 +0200

    Update sq-panel.tsx
    added launch-page link

--- pages/sq-panel.tsx
+++ pages/sq-panel.tsx
@@ -3480,7 +3480,7 @@ export const SqPanel = ({ isMobile }: { isMobile: boolean }) => {
                 from our community. To get early news, insights and potentially
                 get a free Eval-Kit for early bird testing of new features, pls
                 subscribe{' '}
-                <Link href="/sq-panel-launchpage" className="text-success">
+                <Link href="/product/sq-panel/launch-page" className="text-success">
                   <strong className="text-success">here </strong>
                 </Link>
                 and stay tuned.
