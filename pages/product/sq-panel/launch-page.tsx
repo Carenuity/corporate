17b45f8bd4b8ced27d4ffdcbee3784bfa26a97dd
mevica <mevicandanu@gmail.com>
2025-07-01 16:32:42 +0800

    feat: updating launch-page

--- pages/product/sq-panel/launch-page.tsx
+++ pages/product/sq-panel/launch-page.tsx
@@ -100,10 +100,13 @@ const Page = ({ isMobile }: { isMobile: boolean }) => {
                 <div className="col-lg-2 mb-4">
                   {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="/img/content/sq-panel-plus/launch.jpeg" width="162" height="150" alt="SQ-UseCases"
+                src="/img/content/sq-panel-plus/launch.jpeg" 
+                width="162" 
+                height="150" 
+                alt="SQ-UseCases"
                 className=" wow fadeInUp"
                 data-wow-delay="100ms"
-                alt="..."
+               
                 style={{
                   visibility: 'visible',
                   animationDelay: '100ms',
