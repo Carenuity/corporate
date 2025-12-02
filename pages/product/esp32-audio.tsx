7a2b13e02d11f594f1416b1df1876aa71e5cfb30
mevica <mevicandanu@gmail.com>
2025-12-02 12:41:08 +0300

    feat: adding product pages

--- pages/product/esp32-audio.tsx
+++ pages/product/esp32-audio.tsx
@@ -165,7 +165,7 @@ const Index = () => {
           <div id="subscribe" className="my-5 " style={{ textAlign: 'center' }}>
             <h2>Product Wishlist</h2>
             <p>
-              Join <strong>Envirosence 680</strong> wishlist for updates when
+              Join <strong>ESP32 Audio</strong> wishlist for updates when
               ready
             </p>
             <SubscriptionWidget categoryId={3} />
--- pages/product/magnetic-traffic-counting.tsx
+++ pages/product/magnetic-traffic-counting.tsx
@@ -293,7 +293,7 @@ const Index = ({ isMobile }: { isMobile: boolean }) => {
           <div id="subscribe" className="my-5 " style={{ textAlign: 'center' }}>
             <h2>Product Wishlist</h2>
             <p>
-              Join <strong>Envirosence 680</strong> wishlist for updates when
+              Join <strong>Magnetic Traffic Counting</strong> wishlist for updates when
               ready
             </p>
             <SubscriptionWidget categoryId={3} />
