e194b6e513ec15534b9a154f8c173c07a1288292
mevica <mevicandanu@gmail.com>
2026-04-07 14:23:55 +0300

    feat: updating c3-mini-kisii-boxes

--- pages/product/assortment-box/c3-mini-kisii-boxes.tsx
+++ pages/product/assortment-box/c3-mini-kisii-boxes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Head from 'next/head';
 import PageTitle from '../../../components/PageTitle';
 
-
 const Page = () => {
   const pageTitle = 'IoT & AI Sensor Kits';
 
